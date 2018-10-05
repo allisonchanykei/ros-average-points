c4e271e36aa200eb254a49e0156b9c5c8ed0ea0f
Allison CHan <allisonchanykei@gmail.com>
2018-10-04 22:32:25 -0400

    calculates average

--- src/calculate_average.py
+++ src/calculate_average.py
@@ -3,17 +3,26 @@
 import rospy
 from avg_points.msg import Point
 
-#def talker():
+# def talker():
+
 
 def callback(data):
-    rospy.loginfo('x: %2f, y: %2f', data.x, data.y)
+    callback.sumx += data.x
+    callback.sumy += data.y
+    callback.n += 1
+    rospy.loginfo('n: %d', callback.n)
+    rospy.loginfo('x: %2f, y: %2f', callback.sumx/callback.n, callback.sumy/callback.n)
+
 
 def listener():
+    callback.sumx = callback.sumy = callback.n = 0
+
     rospy.init_node('average_calculator', anonymous=True)
     rospy.Subscriber('random_point', Point, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()
