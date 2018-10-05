298240f75531848120679e04d98672a70ada1050
Allison CHan <allisonchanykei@gmail.com>
2018-10-04 22:32:35 -0400

    fixed service error

--- src/generate_points.py
+++ src/generate_points.py
@@ -4,20 +4,23 @@ import rospy
 from avg_points.msg import Point
 from random import uniform
 
+
 def generate_number():
-    return uniform(-100,100)
+    return uniform(-100, 100)
+
 
 def talker():
     pub = rospy.Publisher('random_point', Point, queue_size=10)
-    rospy.init_node('point_generator ', anonymous=True)
-    rate = rospy.Rate(1) # 1hz
+    rospy.init_node('point_generator', anonymous=True)
+    rate = rospy.Rate(1)  # 1hz
     while not rospy.is_shutdown():
         point = Point()
         point.x = generate_number()
         point.y = generate_number()
-        #rospy.loginfo('x: %2f, y: %2f', point.x, point.y)
+        rospy.loginfo('x: %2f, y: %2f', point.x, point.y)
         pub.publish(point)
         rate.sleep()
+
 
 if __name__ == '__main__':
     try:
